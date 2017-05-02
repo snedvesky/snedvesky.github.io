c092cd66630cbc997fc2a984b64d776a9f3a9173
snedvesky <thenuggetry@Scotts-MBP.domain>
2017-05-01 22:55:13 -0600

    Final recipejs update

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -1,13 +1,19 @@
+//edited by Denise 4/29/17 1500
+//added max/min to ingredient list
+//removed unused vars ie:measurement and ingredient:name, quantity
+
+
 var mongoose = require('mongoose');
 var RecipeSchema = new mongoose.Schema({
-    title: {type: String, required: true, unique: true }, // Kelsey: made unique names a requirement
+    title: String,
     postedBy: String,
-    ingredients: [String], // changed by kelsey
-    direction: String // changed by kelsey - should this be an array or single value?
+    ingredients: { type: Array, min: 3, max: 10 },
+    direction: String
+
+
 });
 
 // return the model
 module.exports = mongoose.model('Recipe', RecipeSchema);
-
 
 
